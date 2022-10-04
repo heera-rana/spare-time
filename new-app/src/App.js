9de111e2f6c4456c4f00856f9f9daa7438fc8673
maria-dp <c2061968@hallam.shu.ac.uk>
2022-10-04 13:42:33 +0100

    new directory

---
+++ new-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+//this is our landing page
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       Spare Time
+      </header>
+    </div>
+  );
+}
+
+export default App;
