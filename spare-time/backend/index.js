b9067c8b9a8157d311f4dd8125598c9e3d7309bf
Heera Rana <heera.rana2@skybettingandgaming.com>
2022-10-14 13:03:20 +0100

    installed json server - array appears on webpage

--- spare-time/backend/index.js
+++ spare-time/backend/index.js
@@ -20,38 +20,39 @@ app.listen(port,() => console.log(`Listening on port ${port}`))
 // connect database 
 
 app.get('/events', (req, res) => {
-// const MongoClient = require('mongodb').MongoClient
-// const url = 'mongodb://127.0.0.1:27017'
+const MongoClient = require('mongodb').MongoClient
+const url = 'mongodb://127.0.0.1:27017/events.eventsCollection'
 
-// MongoClient.connect(
-//     url,
-//     {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true
-//     },
-//     (err, client) => {
-//       if (err) {
-//         return console.log(err)
-//       }
+MongoClient.connect(
+    url,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    },
+    (err, client) => {
+      if (err) {
+        return console.log(err)
+      }
     
-//     const db = client.db('events')
+    const db = client.db('events')
   
-//     console.log(`MongoDB Connected: ${url}`)
+    console.log(`MongoDB Connected: ${url}`)
 
-//     const events = db.collection('eventsCollection')
+    // const events = db.collection('events').find({ObjectId});
+    // console.log(events);
+    // res.send(events);
 
-//     db.collectionName.find(eventsCollection)
-
-    res.send("hello world")
-      
-    }
-  )
+    const ObjectId = require("mongodb").ObjectId; 
+    const eventId = new ObjectId('633d3e937accac34e3d72eb2')
+    const events = db.collection('eventsCollection').find({ _id: eventId });
+    console.log(events);
+    res.send(events);
+    
+    })
 
     // db.collectionName.find()
 
     //format the data as you want it
 
     // res.send(data)
-// })
-
-
+  })
--- spare-time/frontend/src/Events.js
+++ spare-time/frontend/src/Events.js
@@ -2,22 +2,33 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function Events () {
-    useEffect(() => {
-        fetchItems();
-    }, []);
-    const [items, setItems] = useState([]);
+//     useEffect(() => {
+//         fetchItems();
+//     }, []);
+//     const [items, setItems] = useState([]);
 
-    const fetchItems = async () => {
+//     const fetchItems = async () => {
         
-    const data = await fetch("https://localhost:3000/events");
-    const items = await data.json();
-    console.log(items);
-    setItems(items);
+//     const data = await fetch("https://localhost:3000/events");
+//     const items = await data.json();
+//     console.log(items);
+//     setItems(items);
+// }
+
+    const [data, setData] = useState(null);
+    fetch("https://localhost:3000/events")
+    .then((res) => {
+if (res.ok) {
+    return res.json();
 }
+    throw new Error("Server says bad response");
+    })
+    .then((res) => setData(res.data))
+    .catch((err) => console.log(err));
 
     return (
         <div className = "App">
-            <h1>Events Page</h1>
+            <h1>[data.map((events)]</h1>
         </div>
     );
 };
