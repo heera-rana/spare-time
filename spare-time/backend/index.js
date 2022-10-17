1fc23e70badecb5312b447314bef50b633c38704
Heera Rana <heera.rana2@skybettingandgaming.com>
2022-10-17 21:33:31 +0100

    connected backend to database

--- spare-time/backend/index.js
+++ spare-time/backend/index.js
@@ -1,59 +1,50 @@
-const express = require("express")
+const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const mongoose= require("mongoose");
+const mongoose = require("mongoose");
 
-const app = express()
+const app = express();
 
 app.use(
-    cors({
+  cors({
     origin: "http://localhost:3000/",
-}))
+  })
+);
 
 app.use(bodyParser.json());
 
 const port = 3000;
 
-app.listen(port,() => console.log(`Listening on port ${port}`))
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
+// todo: refactor mongoClient.connect into its own function to reuse across other routes
 
-// connect database 
+app.get("/events", (req, res) => {
+  const MongoClient = require("mongodb").MongoClient;
+  const url = "mongodb://127.0.0.1:27017/events";
 
-app.get('/events', (req, res) => {
-const MongoClient = require('mongodb').MongoClient
-const url = 'mongodb://127.0.0.1:27017/events.eventsCollection'
-
-MongoClient.connect(
+  MongoClient.connect(
     url,
     {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
     },
-    (err, client) => {
+    async (err, client) => {
       if (err) {
         console.log(err);
-        return console.log(err)
+        return console.log(err);
       }
-    
-    const db = client.db('events')
-  
-    console.log(`MongoDB Connected: ${url}`)
 
-    // const events = db.collection('events').find({ObjectId});
-    // console.log(events);
-    // res.send(events);
+      const db = client.db();
 
-    const ObjectId = require("mongodb").ObjectId; 
-    const eventId = new ObjectId('633d3e937accac34e3d72eb2')
-    const events = db.collection('eventsCollection').find({ _id: eventId });
-    console.log(events);
-    res.send(events);
-    
-    })
-
-    // db.collectionName.find()
-
-    //format the data as you want it
-
-    // res.send(data)
-  })
+      if (db) {
+        const collections = await db
+          .collection("eventsCollection")
+          .find()
+          .toArray();
+        console.log(collections);
+        res.send(collections);
+      }
+    }
+  );
+});
