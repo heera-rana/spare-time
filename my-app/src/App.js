8020841875594eaed188be8323e1d26b820efda6
RachPia <rachael.lawlor@skybettingandgaming.com>
2022-10-20 14:51:38 +0100

    changed div to make neater

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -48,12 +48,12 @@ function App() {
             <div Class="eventImages">
               <div>
                 <div><img src= "eventImages/Class.jpg" alt= "Class"></img></div>
+                <div><img src= "eventImages/Fitness.jpg" alt= "Fitness"></img></div>
+                <div><img src= "eventImages/Random.jpg" alt= "Misc"></img></div>
+                <div><img src= "eventImages/Night out.jpg" alt= "Evening"></img></div>
+               <div><img src= "eventImages/Child friendly.jpg" alt= "Child friendly"></img></div>
               </div>
-              <div><img src= "eventImages/Fitness.jpg" alt= "Fitness"></img></div>
             </div>
-            <div><img src= "eventImages/Random.jpg" alt= "Misc"></img></div>
-            <div><img src= "eventImages/Night out.jpg" alt= "Evening"></img></div>
-            <div><img src= "eventImages/Child friendly.jpg" alt= "Child friendly"></img></div>
           </main>
         </div>
       </body>
