314dd0a88c1e121dacfb73d97c3bd40139e4537f
RachPia <rachael.lawlor@skybettingandgaming.com>
2022-10-24 21:44:40 +0100

    added slide show

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,6 @@
 
 function Home() {
-  return (
-    <div>
-        <div>
-                <div><img src= "eventImages/Classes and Workshops.jpg" alt= "Class"></img></div>
-                <div><img src= "eventImages/Sports and Fitness.jpg" alt= "Fitness"></img></div>
-                <div><img src= "eventImages/Misc.jpg" alt= "Misc"></img></div>
-                <div><img src= "eventImages/Evening Events.jpg" alt= "Evening"></img></div>
-               <div><img src= "eventImages/Childrens Events.jpg" alt= "Child friendly"></img></div>
-               <div><img src= "eventImages/Music Events.jpg" alt= "Music"></img></div>
-              </div> 
-    </div>
-  )
+  
 }
 
 export default Home
