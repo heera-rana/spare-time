457402c671549f129558953d6877399b348e05f7
Maria Diome-Pena <maria.diome-pena@skybettingandgaming.com>
2022-11-03 17:38:46 +0000

    event components

--- frontend/src/pages/EventDetails.jsx
+++ frontend/src/pages/EventDetails.jsx
@@ -1,24 +1,16 @@
-import React from "react";
-import {useLocation} from 'react-router-dom';
-
-
+import { useLocation, useNavigate } from "react-router-dom";
+import OneEvent from "../components/OneEvent";
 
 function EventDetails (props) {
     const eventData = useLocation();
-    //console.log(eventData)
+    const navigate = useNavigate()
 
     return (
-    <div>
-    <h3>{eventData.state.categories}</h3>
-    <img src= {eventData.state.image} />
-    <p>{eventData.state.time}</p>
-    <p>{eventData.state.provider}</p>
-    <p>{eventData.state.duration}</p>
-    <p>{eventData.state.price}</p>
-    <p>{eventData.state.description}</p>
-    </div>
-);
-
+        <div>
+            <OneEvent eventData={eventData} />
+            <button onClick={() => navigate('/events')} >go back</button>
+        </div>
+    );
 }
-
+    
 export default EventDetails;
