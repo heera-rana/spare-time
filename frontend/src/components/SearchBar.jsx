5816255b1f290ca77933865dce6c7fdca1eaada8
Maria Diome-Pena <maria.diome-pena@skybettingandgaming.com>
2022-11-09 09:35:24 +0000

    popup messages in login

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -4,36 +4,36 @@ import './SearchBar.css';
 
 function SearchBar ({placeholder, data}){
 
-        const[filteredData, setFilteredData] = useState([]);
+    const[filteredData, setFilteredData] = useState([]);
 
-        const handleFilter = (event) => {
-           const searchWord = event.target.value
-            const newFilter = data.filter ((value) => {
-                return value.description.toLowerCase().includes (searchWord.toLowerCase());
-            });
-            if (searchWord === "") {
-                setFilteredData ([]);   
-            } else {
-                setFilteredData(newFilter);
-            }
-        };
+    const handleFilter = (event) => {
+        const searchWord = event.target.value
+        const newFilter = data.filter ((value) => {
+            return value.description.toLowerCase().includes (searchWord.toLowerCase());
+        });
+        if (searchWord === "") {
+            setFilteredData ([]);   
+        } else {
+            setFilteredData(newFilter);
+        }
+    };
     return (
         <div className='search'>
             <div className='searchInputs'>
                 <input type= "text" placeholder={placeholder} onChange={handleFilter}/>
-                </div>
+            </div>
 
             {filteredData.length !== 0 && (
-              <div className='dataResult'>
+            <div className='dataResult'>
                 {filteredData.slice(0,15).map((value, key) =>{
                     return (
                         <a className="dataItem" href= "Events" target="_blank"> 
-                         <p>{value.description}{" "}</p>
+                        <p>{value.description}{" "}</p>
                         </a>
                     );
                 })}
             </div>
-)}
+            )}
         </div>
     )
 }
