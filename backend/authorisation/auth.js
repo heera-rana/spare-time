5fa0074a97d42a3d528f27298b7074757bd8e748
Ryan Sutcliffe <ryan.sutcliffe@skybettingandgaming.com>
2022-11-09 12:05:53 +0000

    changes to auth

--- backend/authorisation/auth.js
+++ backend/authorisation/auth.js
@@ -1,15 +1,15 @@
 const jwt = require('jsonwebtoken')
 const asyncHandler = require('express-async-handler')
-const Event = require ('../models/eventModels')
+const User = require ('../models/userModels')
 
 
-const protectAddEvent = asyncHandler(async (req, res, next) => {
+const protect = asyncHandler(async (req, res, next) => {
     let token 
     if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         try {
             token = req.headers.authorization.split(' ')[1] //this get the token from the header ansd then splits the string the space is important
             const decoded = jwt.verify(token, process.env.JWT_SECRET)
-            req.event = await Event.findById(decoded.id).select('-password')
+            req.user = await User.findById(decoded.id).select('-password')
 
             next()
         } catch (error) {
@@ -18,11 +18,11 @@ const protectAddEvent = asyncHandler(async (req, res, next) => {
             throw new Error ('No access granted')
         }
     }
-    if(!token ) {
+    if(!token) {
         console.log(error)
         res.status(401)
         throw new Error ('No access granted')
     }
 })
 
-module.exports = { protectAddEvent }
+module.exports = { protect }
