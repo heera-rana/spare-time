d2e483b8b38c00f909a25968b60a0ede3c77824d
Ryan Sutcliffe <ryan.sutcliffe@skybettingandgaming.com>
2022-11-09 15:29:04 +0000

    Updated the ogin code as it was updated with an older version and inserted the login alerts from SWAL

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,53 +3,49 @@ import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2"
 
 function Login (){
-    const [user, setUser] = useState([]);
-    const navigate = useNavigate()
-    
-    function updateUser(value){
-        return setUser((prev) => {
-            return {...prev, ...value}
-        })
-    }
-    
-    async function onSubmit(e){
-        e.preventDefault()
 
-        const newUser = {...user}
+const navigate = useNavigate()
+
+async function loginUser(userData) {
+    return fetch("http://localhost:5000/api/users/login", {
+        method: "POST",
+        mode:"cors",
+        headers: {
+            "Content-Type": "application/json"
+           
+        },
+        body: JSON.stringify(userData),
+    })
+    .then((response)=>{
+        if (response.status === 200){
+            Swal.fire({
+                icon: 'success',
+                title: 'Yay',
+                text: 'you are successfully logged in',
+              })
+            navigate("/")
+        } else {
+            var error = response.status
+            console.log(error)
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'wrong username or password',
+                footer: '<a href="">Why do I have this issue?</a>'
+              })
+        }
+    })
+}
+         const [email, setEmail] = useState({});
+         const [password, setPassword] = useState({});
     
-        await fetch("http://localhost:5000/api/users/login", {
-            method: "POST",
-            mode:"cors",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(newUser),
-        })
-        .then((response)=>{
-            if (response.status === 200){
-                // response.json()
-                Swal.fire({
-                    icon: 'success',
-                    title: 'Yay',
-                    text: 'you are successfully logged in',
-                  })
-                navigate("/")
-            } else {
-                var error = response.status
-                console.log(error)
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Oops...',
-                    text: 'wrong username or password',
-                    footer: '<a href="">Why do I have this issue?</a>'
-                  })
-            }
-        })
-        setUser([])
-           //if there is a reponse status of 200 then successful login, reset the form and nav to home page, console.log the recieved token
-           //else if user login failed invalid credentials and display the invalid credatial in the console
-           //save the JWT in local storage 
-    } 
+         const onSubmit = async e => {
+           e.preventDefault();
+        loginUser({
+             email,
+             password,
+           }); 
+         } 
     return (
         <div>
             <h1>Login</h1>
@@ -60,7 +56,7 @@ function Login (){
                     type="email"
                     className="form-control"
                     id="email"
-                    onChange={(e) => updateUser({email: e.target.value})}
+                    onChange={e => setEmail(e.target.value)}
                     required
                     />
                 </div>
@@ -70,7 +66,7 @@ function Login (){
                     type="password"
                     className="form-control"
                     id="password"
-                    onChange={(e) => updateUser({password: e.target.value})}
+                    onChange={e => setPassword(e.target.value)}
                     required
                     password='true'
                     />
