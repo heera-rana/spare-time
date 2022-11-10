3c4ec5ae9e47dde6bb56b4034a1010b212cb6a85
Ryan Sutcliffe <ryan.sutcliffe@skybettingandgaming.com>
2022-11-10 15:31:41 +0000

    JSON mongodb

--- backend/config/dbconnection.js
+++ backend/config/dbconnection.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 //creates a fuction called connectDB(), this 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect('mongodb://localhost:27017/SHU')
+        const conn = await mongoose.connect('mongodb://localhost:27017/spare-time')
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.log(`Error: ${error.message}`);
