fb81fbe11bfa02846a3fb688127cbf2fedb62df9
Heera Rana <heera.rana2@skybettingandgaming.com>
2022-11-14 22:34:04 +0000

    cleaning filter code

--- backend/config/dbconnection.js
+++ backend/config/dbconnection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 //creates a fuction called connectDB()
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://localhost:27017/SHU");
+    const conn = await mongoose.connect("mongodb://localhost:27017/spare-time");
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(`Error: ${error.message}`);
