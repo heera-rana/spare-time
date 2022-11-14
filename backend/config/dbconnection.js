756d47b89238b3ecbbc7718209d2826d272c7c74
Ryan Sutcliffe <ryan.sutcliffe@skybettingandgaming.com>
2022-11-14 14:48:02 +0000

    minor changes

--- backend/config/dbconnection.js
+++ backend/config/dbconnection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 
-//creates a fuction called connectDB(), this 
+//creates a fuction called connectDB()
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect('mongodb://localhost:27017/spare-time')
