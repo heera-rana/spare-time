f55b58872bab8194ff0f958f52c54bf789a00a8c
Maria Diome-Pena <maria.diome-pena@skybettingandgaming.com>
2022-11-15 10:32:32 +0000

    hiding add event page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,9 +2,39 @@ import whiteMenu from "../images/whiteMenu.svg";
 import { Link } from "react-router-dom";
 import SearchBar from "./SearchBar";
 import EventData from './Data.json'
+import {useState, useEffect} from "react"
 
+const Navigation = ({ token, onLogout }) => {
+  console.log(token)
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/addEvent">Add Event</Link></li>
+        <li><Link to="/signUp">Sign Up</Link></li>
+        <li><Link to="/events">Events</Link></li>
+      </ul>
+
+      {token && (
+        <button type="button" onClick={onLogout}>
+          Sign Out
+        </button>
+      )}
+    </nav>
+  );
+};
 
 function Header() {
+  const [token, setToken] = useState([]);
+
+  useEffect(() => {
+    const token = (localStorage.getItem('token'));
+    if (token) {
+    setToken(token);
+    }
+  }, [])
+
   return (
           <header className="App-header">
                 <div>
@@ -16,15 +46,7 @@ function Header() {
                 <div className="burgerMenuContainer">
                   <img src={whiteMenu} alt="Menu" width="30px" id="burgerMenu"/>  
                 </div>
-                <nav>
-                  <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/login">Login</Link></li>
-                    <li><Link to="/addEvent">Add Event</Link></li>
-                    <li><Link to="/signUp">Sign Up</Link></li>
-                    <li><Link to="/events">Events</Link></li>
-                  </ul>
-                </nav>
+                <Navigation token={token}/>
             </header>
           )}
 
