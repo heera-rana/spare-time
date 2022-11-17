50c7a5d543e7947c9fb13870dba4e7662c629efa
Maria Diome-Pena <maria.diome-pena@skybettingandgaming.com>
2022-11-17 14:53:47 +0000

    form gets submitted but needs to update database

--- frontend/src/pages/EventDetails.jsx
+++ frontend/src/pages/EventDetails.jsx
@@ -1,13 +1,23 @@
 import { useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router-dom"
+import EventForm from "../components/EventForm"
 import OneEvent from "../components/OneEvent"
 
 function EventDetails () {
     const [oneEvent, setOneEvent] = useState([])
     const [token, setToken] = useState([])
+    const [isPending, setIsPending] = useState(false)
     const navigate = useNavigate()
     const params = useParams()
     const id = params.id
+    const buttonLabel = ["Update event", "Updating event"]
+
+
+    function updateEvent(value){
+        return setOneEvent((prev) => {
+            return {...prev, ...value}
+        })
+    } 
 
     useEffect(() => {
         const token = (sessionStorage.getItem('token'))
@@ -51,13 +61,41 @@ function EventDetails () {
         })
     }
 
+    
+    const onSubmit = async () =>{
+        setIsPending(true)
+
+        const data = await fetch(`http://localhost:5000/api/events/oneEvent/${id}`,{
+            method: "POST",
+            mode:"cors",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`
+            },
+        })
+        if (data) {
+            const response = await data.json()
+           // response.image = `eventImages/${response.categories}.jpg`
+            //setOneEvent(response);
+            setIsPending(false)
+        } else {
+            console.log("No events found.");
+        }
+        onSubmit().catch(console.error)
+        setOneEvent([]);
+    }
+
     return (
         <div>
             <OneEvent eventData={oneEvent} />
             <button onClick={() => navigate('/')} >go back</button>
             {isLoggedIn(token) && 
-                <button onClick={()=>deleteEvent()}>delete</button>
-           }
+                <div>
+                    <button onClick={()=>deleteEvent()}>delete</button>
+                    {/* <button onClick={()=>()}>edit</button> */}
+                    <EventForm event={oneEvent} onSubmit={onSubmit} setIsPending={setIsPending} updateEvent={updateEvent} buttonLabel={buttonLabel} title={"Edit event"}/>
+                </div>
+            }
         </div>
     )
 }
--- frontend/src/pages/NewEvent.jsx
+++ frontend/src/pages/NewEvent.jsx
@@ -2,11 +2,14 @@ import {useState, useEffect} from "react"
 import { useNavigate } from "react-router-dom"
 import Swal from "sweetalert2"
 import '../CSS/form.css'
+import EventForm from "../components/EventForm"
 
 function NewEvent (){  
     const [event, setEvent] = useState([])
     const [isPending, setIsPending] = useState(false)
     const navigate = useNavigate()
+    const buttonLabel = ["Add new event", "Adding new event"]
+    console.log(buttonLabel)
 
 
     function updateEvent(value){
@@ -64,108 +67,8 @@ function NewEvent (){
 
     return (
         <div>
-            <div className= "form">
-            <h2>Add new event</h2>
-            <form onSubmit={onSubmit}>
-                <label className="form-group"> Event Title: </label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        id="title"
-                        value={event.title || ""}
-                        onChange={(e)=>updateEvent({title: e.target.value})}
-                        required
-                    />
-                
-                <label className="form-group"> Category: </label>
-                    <select 
-
-                        value={event.categories} 
-                        className="form-control" 
-                        onChange={(e)=>updateEvent({categories: e.target.value})}
-                        required
-                        defaultValue={'DEFAULT'} 
-                    >
-                        <option value="DEFAULT" disabled>--- Select category ---</option>
-                        <option value= "Childrens Events">Childrens Events</option>
-                        <option value="Classes and Workshops">Classes and Workshops</option>
-                        <option value="Evening Events">Evening Events</option>
-                        <option value="Misc">Misc</option>
-                        <option value="Music Events">Music Events</option>
-                        <option value="Sports and Fitness">Sports and Fitness</option>
-                    </select>
-
-                <label className="form-group"> Provider: </label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        id="provider"
-                        value={event.provider || ""}
-                        onChange={(e)=>updateEvent({provider: e.target.value})}
-                        required
-                    />
-               
-                <label className="form-group"> Date: </label>
-                    <input
-                        type="datetime-local"
-                        className="form-control"
-                        id="date"
-                        value={event.time}
-                        onChange={(e)=>updateEvent({date: e.target.value})}
-                        required
-                    />
-
-                <label className="form-group"> Availability: </label>
-                    <input
-                        type="number"
-                        className="form-control"
-                        id="availability"
-                        value={event.availability || ""}
-                        onChange={(e)=>updateEvent({availability: e.target.value})}
-                        required
-                    />
-
-                <label className="form-group"> Duration: </label>
-                    <input
-                        type="number"
-                        className="form-control"
-                        id="duration"
-                        value={event.duration || ""}
-                        onChange={(e)=>updateEvent({duration: e.target.value})}
-                        required
-                        placeholder="minutes"
-                        min = "10"
-                        step = "5"
-                    />
-                
-                <label className="form-group"> Price: </label>
-                    <input
-                        type="number"
-                        className="form-control"
-                        id="price"
-                        value={event.price || ""}
-                        onChange={(e)=>updateEvent({price: e.target.value})}
-                        step="0.01"
-                        required
-                    />
-
-                <label className="form-group">Description: </label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        id="description"
-                        value={event.description || ""}
-                        onChange={(e)=>updateEvent({description: e.target.value})}
-                        required
-                    />
-
-                    <div className="button">
-                { !isPending && <button> Add new Event</button>}
-                { isPending && <button disabled> Adding new Event ...</button>}
-                </div>
-            </form>
-            </div>
-        </div>
+            <EventForm event = {event} isPending={isPending} onSubmit={onSubmit} updateEvent={updateEvent} buttonLabel = {buttonLabel} title={"Add new Event"}/>
+        </div>               
     )
 }
   
